6c7e9f590c8e011b5482deca9022b1a99cb62201
Mario Andrés Zaldívar de la Cruz <marioandreszaldivar876@gmail.com>
2024-02-29 00:28:14 +0100

    Added second scene link

---
+++ Shooting Wizard/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public SceneAsset newScene;
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            ChangeScene();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene(newScene.name);
+    }
+}
